cd5318a32be2b94f105abd7bc19dfae39772c1bc
Pavan Gangadhara <pavan.gangadhara06@gmail.com>
2020-09-29 15:38:04 +0530

    second commit

--- react-todo-list-app/src/App.js
+++ react-todo-list-app/src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
-// showing vs-code github setup
+// showing vs-code git-hub setup
 
 class App extends React.Component {
   render(){
